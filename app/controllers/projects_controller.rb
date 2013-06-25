dbb6dce40a213d0935a006e8145cf174e7fdfd24
iliis <samuelbryner@gmx.ch>
2013-06-25 20:40:05 +0200

    Added create project form.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,18 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(params[:project])
+
+    if @project.save
+      redirect_to :action => :index
+    else
+      render :action => :new
+    end
+  end
 end
